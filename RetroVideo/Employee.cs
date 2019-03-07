2ffd3fa42a99b148f519004229d4d7d73b0f9fa9
unknown <jonathanmohan.net@gmail.com>
2019-03-07 00:37:28 -0600

    update

---
+++ RetroVideo/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RetroVideo
+{
+    public class EmployeeInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public void AddFirstName(string firstName)
+        {
+            FirstName = firstName;
+        }
+
+        public void AddLastName(string lastName)
+        {
+            LastName = lastName;
+        }
+
+        public void AddTitle(string title)
+        {
+            Title = title;
+        }
+
+        public void AddStartDate(DateTime startDate)
+        {
+            StartDate = startDate;
+        }
+
+        public EmployeeInfo(string lastName, string firstName, string title, DateTime startDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Title = title;
+            StartDate = startDate;
+        }
+    }
+}
--- RetroVideo/Program.cs
+++ RetroVideo/Program.cs
@@ -5,35 +5,39 @@ namespace RetroVideo
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome To Retro Video!");
-        }
 
         public class Company
         {
+
             /*
-                Some readonly properties
+            Some readonly properties
             */
+
             public string CompanyName { get; }
             public DateTime CreatedOn { get; }
 
             // Create a property for holding a list of current employees
 
-            public List<EmployeeInfo> employees { get; set; }
+            public List<EmployeeInfo> Employees { get; set; }
 
             // Create a method that allows external code to add an employee
 
             public void AddEmployee(EmployeeInfo newEmployee)
             {
-                employees.Add(newEmployee);
+                Employees.Add(newEmployee);
             }
 
             // Create a method that allows external code to remove an employee
 
             public void RemoveEmployee(EmployeeInfo exEmployee)
             {
-                employees.Remove(exEmployee);
+                Employees.Remove(exEmployee);
+            }
+
+            public void listEmployees(List<EmployeeInfo> employees)
+            {
+                foreach (EmployeeInfo employee in employees)
+                    Console.WriteLine(employee.FirstName);
             }
 
             /*
@@ -46,7 +50,35 @@ namespace RetroVideo
             {
                 CompanyName = companyName;
                 CreatedOn = createdOn;
+                Employees = new List<EmployeeInfo>();
 
+            }
+
+            public void ListEmployees(List<EmployeeInfo> employees)
+            {
+                foreach (EmployeeInfo employee in employees)
+                    Console.WriteLine(employee.FirstName, employee.LastName);
+            }
+
+            static void Main(string[] args)
+            {
+                Company RexBot = new Company("Rex Bot", DateTime.Now);
+
+                EmployeeInfo rex = new EmployeeInfo("Davis", "Rex", "Owner", DateTime.Now);
+                EmployeeInfo bot = new EmployeeInfo("Bot", "Ro", "Computer", DateTime.Now);
+                EmployeeInfo gunner = new EmployeeInfo("Nelson", "Gunner", "Manager", DateTime.Now);
+
+                RexBot.AddEmployee(rex);
+                RexBot.AddEmployee(bot);
+                RexBot.AddEmployee(gunner);
+
+                Console.WriteLine("Welcome To Rex Bot's Employee Vault");
+                Console.WriteLine("Hit Any Key To See List.");
+                Console.ReadKey();
+
+                RexBot.listEmployees(RexBot.Employees);
+                Console.WriteLine("Hit Any Key To Exit.");
+                Console.ReadKey();
             }
         }
     }
